d2778a33dcb419189c6de8a57067d5e08185e865
Akash Jadhav <akash.jadhav.cse@gmail.com>
2020-08-06 22:11:27 +0530

    copy homecontroller from project-"Basics"

---
+++ IdentityExample/Controller/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize] //are u allowed to come heres?
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var localAuthorityClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Akash"),
+                new Claim(ClaimTypes.Email, "akash.jadhav.cse@gmail.com"),
+                new Claim("Nickname", "gambitier")
+            };
+
+            var licenceAuthorityClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Akash.Laxman.Jadhav"),
+                new Claim(ClaimTypes.Email, "csegambitier@gmail.com"),
+                new Claim("Nickname", "AJ"),
+                new Claim("Licence.Number", "IN9763714604")
+            };
+
+            var localAuthorityIdentity = new ClaimsIdentity(localAuthorityClaims, "Local Authority's Identity");
+
+            var licenceAuthorityIdentity = new ClaimsIdentity(licenceAuthorityClaims, "Liecence Authority's Identity");
+
+            var appAuthenticationPrincipal = new ClaimsPrincipal(new[] { localAuthorityIdentity, licenceAuthorityIdentity });
+
+            HttpContext.SignInAsync(appAuthenticationPrincipal);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
